0fb0b18bdcb0b41b32689a9f3bfa7b6eb4a7f0ec
helloukey <kunalukey32@gmail.com>
2022-07-12 12:11:40 +0530

    added chrome extension availability, removed dayjs cdn and added locally.

--- index.js
+++ index.js
@@ -141,7 +141,7 @@ const getWeather = async (latitude, longitude) => {
 };
 // Update Weather
 const updateWeather = (response) => {
-  weatherIcon.src = response.current.condition.icon;
+  weatherIcon.src = `https:${response.current.condition.icon}`;
   locationText.textContent = response.location.name;
   const tempText = `${response.current.temp_c}° C`;
   temperatureText.textContent = tempText;
